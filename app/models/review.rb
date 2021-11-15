23c9ee341eb23c1dd4a06d1947bdf3adfe98ab01
anonymous anonymous <juliejychung@gmail.com>
2021-11-15 20:46:59 +0000

    Direct association between users and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  belongs_to :commenter,
+             :class_name => "User"
+
   belongs_to :movie
 
   # Indirect associations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :foreign_key => "commenter_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
